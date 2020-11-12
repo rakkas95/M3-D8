2f44c91363a152396cb1b9906b58d94dc59a90ef
rakkas95 <rakkas95@users.noreply.github.com>
2020-11-12 15:14:18 +0000

    some part of yesterday's homework completed

---
+++ backoffice.js
@@ -0,0 +1,64 @@
+const url = "https://striveschool-api.herokuapp.com/api/product/"
+
+window.onload = async () => {
+    let urlParams = new URLSearchParams(window.location.search);
+    id = urlParams.get("id");
+    if (id) {
+        let response = await fetch(url + id, {
+            "headers": {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFiZTc1NDRiY2RlMTAwMTc2MTZhYmQiLCJpYXQiOjE2MDUxMDEzOTYsImV4cCI6MTYwNjMxMDk5Nn0.4L61-jzRzehBUMfWi2WMJ8HBnpYAZyKtTPtM7wrzvyk"
+            }
+        });
+        let product = await response.json();
+        document.getElementById("name").value = product.name;
+        document.getElementById("description").value = product.description;
+        document.getElementById("brand").value = product.brand;
+        document.getElementById("imageUrl").value = product.imageUrl;
+        document.getElementById("price").value = product.price;
+    }
+};
+
+const handleSubmit = (pD) => {
+    pD.preventDefault();
+    submitProduct();
+};
+
+const submitProduct = async () => {
+    let productAdded = {
+        name: document.querySelector("#name").value,
+        description: document.querySelector("#description").value,
+        brand: document.querySelector("#brand").value,
+        imageUrl: document.querySelector("#imageUrl").value,
+        price: document.querySelector("#price").value,
+    };
+
+    try {
+        let response;
+        if (id) {
+            response = await fetch(url + id, {
+                method: "PUT",
+                body: JSON.stringify(productAdded),
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                }),
+            });
+        } else {
+            response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(productAdded),
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                }),
+            });
+        }
+
+        if (response.ok) {
+            alert(`Product ${id ? "updated" : "added"} successfully`);
+            location.assign("index.html");
+        } else {
+            alert("Something went wrong!");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+window.onload = async () => {
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+let productItems = document.getElementById('productItems');
+
+try {
+    let response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        "headers": {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFiZTc1NDRiY2RlMTAwMTc2MTZhYmQiLCJpYXQiOjE2MDUxMDEzOTYsImV4cCI6MTYwNjMxMDk5Nn0.4L61-jzRzehBUMfWi2WMJ8HBnpYAZyKtTPtM7wrzvyk"
+        }
+    });
+    let productData = await response.json();
+    console.log(productData);
+
+    if(productData.length > 0) {
+        productData.forEach( (pD) => {
+            let columnItem = document.createElement('div');
+        columnItem.classList.add("col-xs-12", "col-md-3", 'd-flex', 'justify-content-between');
+        columnItem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src=${pD.imageUrl} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${pD.name}</h5>
+                    <p class="card-text">${pD.description}</span></p>
+                    <span>${pD.brand}</span> <span>${pD.price}
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+        </div>` ;
+        productItems.appendChild(columnItem) ; 
+        });
+    } else {
+        productItems.innerHTML = "<h1>No products found"
+    }
+
+    } catch (error) {
+    alert (error);
+    }
+};
+
+
+
+
